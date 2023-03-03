684a4d46734d34ab75455b79fab0f856c2a7969e
jacksonlevine <jack@netbug.us>
2023-03-03 09:40:53 -0800

    add dispose method to vendor tests calling clearall

--- PierresVendors.Tests/ModelsTests/VendorTests.cs
+++ PierresVendors.Tests/ModelsTests/VendorTests.cs
@@ -1,12 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PierresVendors.Models;
 using System.Collections.Generic;
+using System;
 
 namespace PierresVendors.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
     [TestMethod]
     public void Vendor_CreatesInstanceOfVendor_Vendor()
     {
